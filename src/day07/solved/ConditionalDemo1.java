16d6bee0fbf94ee6f11e138356f40194ed8bc318
Vinit Gore <72271049+Vinit-source@users.noreply.github.com>
2023-06-08 17:51:14 +0530

    Commit demo

---
+++ src/day07/solved/ConditionalDemo1.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package day07.solved;
+
+/**
+ * @author VinitGore
+ *
+ */
+public class ConditionalDemo1 {
+
+}
---
+++ src/day07/solved/ConditionalStatementDemo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package day07.solved;
+
+/**
+ * @author VinitGore
+ *
+ */
+public class ConditionalStatementDemo {
+
+	public static void main(String[] args) {
+		
+	    int marks = 85;
+	    
+        if ( marks >= 75 ) {
+            System.out.println("You got an A grade");
+        }
+        
+
+	}
+
+}
