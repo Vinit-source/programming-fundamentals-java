fea991aa93409bc8d328c3277c10deb827122597
Vinit Gore <72271049+Vinit-source@users.noreply.github.com>
2023-06-09 10:27:32 +0530

    Day 07

--- src/day07/solved/ConditionalDemo1.java
+++ src/day07/solved/ConditionalDemo1.java
@@ -7,6 +7,18 @@ package day07.solved;
  * @author VinitGore
  *
  */
-public class ConditionalDemo1 {
+
+public class ConditionalStatementDemo {
+
+	public static void main(String[] args) {
+		
+	    int marks = 85;
+	    
+        if ( marks >= 75 ) {
+            System.out.println("You got an A grade");
+        }
+        
+
+	}
 
 }
