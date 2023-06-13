3d3340943b68bc9435cb580ce7c777ad00a1596e
Vinit Gore <72271049+Vinit-source@users.noreply.github.com>
2023-06-13 19:17:02 +0530

    Update ArrayDemo2.java

--- src/day09/ArrayDemo2.java
+++ src/day09/ArrayDemo2.java
@@ -1,26 +1,78 @@
-package day09;
+package Day09.Solved;
 
 public class ArrayDemo2 {
 
-	public static void main(String[] args) {
+	String name;
+	int age;
 
-		// Array with a length of 3
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		String[] names = new String[3];
 
-		// assign marks in the array
-		names[0] = "Surya";
-		names[1] = "Vijay";
-		names[2] = "Ajith";
 
-		// find how many items in the array
-		System.out.println("Array size is: " + names.length);
-
-		// Iterate array using for loop
-		for (int i = 0; i < names.length; i++) {
+		for(int i=0; i<names.length;i++) {
 			String m = names[i];
-			System.out.println("Name is: " + m);
+			System.out.println("The name is "+m);
 		}
 
-	}
+		System.out.println("Total Array length is " + names.length);
 
+		for(int i=0; i<names.length;i++) {
+			String m = names[i];
+			System.out.println("The name is "+m);
+		}
+
+//		Character Array
+
+		char[] letters = new char[3];
+
+		for(int i=0; i<letters.length;i++) {
+			char m = letters[i];
+			System.out.println("The letter is "+m);
+			System.out.println((int)m);
+		}
+
+		letters[0] = 'F';
+		letters[1] = 'A';
+		letters[2] = 'N';
+
+
+
+		for(int i=0; i<letters.length;i++) {
+			char m = letters[i];
+			System.out.println("The letter is "+m);
+			System.out.println("The integer value is: " +(int)m);
+		}
+
+//		// object Array
+		ArrayDemo2[] details = new ArrayDemo2[3];  // create array of objects
+		System.out.println(Arrays.toString(details));
+		System.out.println(details[0]);
+
+
+
+		// creating objects
+		ArrayDemo2 student1 = new ArrayDemo2();
+		student1.name ="Batman";
+		student1.age =16 ;
+		ArrayDemo2 student2 = new ArrayDemo2();
+		student2.name ="Superman";
+		student2.age = 17;
+		ArrayDemo2 student3 = new ArrayDemo2();
+		student3.name ="Spiderman";
+		student3.age = 48;
+
+		// assigning the objects into Array
+		details[0] = student1;
+		details[1] = student2;
+		details[2] = student3;
+
+		// loop for printing the value
+		for(ArrayDemo2 e : details) {
+			System.out.println(e);
+			System.out.println("Student name is " + e.name + " he is " + e.age + " yrs old ");
+		};
+
+
+	}
 }
