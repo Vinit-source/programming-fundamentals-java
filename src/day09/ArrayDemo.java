52b89172974ab5d2c632f716450402cd5ebec6a8
Vinit Gore <72271049+Vinit-source@users.noreply.github.com>
2023-06-13 19:19:18 +0530

    Update ArrayDemo.java

--- src/day09/ArrayDemo.java
+++ src/day09/ArrayDemo.java
@@ -1,28 +1,33 @@
-package day09;
+package day09.solved;
 
-public class ArrayDemo {
-
+public class Integer_Array {
 	public static void main(String[] args) {
-
-		// Array with a length of 3
-		int[] marks = new int[3];
-
-		// assign marks in the array
-		marks[0] = 50;
-		marks[1] = 60;
-		marks[2] = 70;
-
-    
-		// find how many items in the array
-        System.out.println("Array size is: " + marks.length);
+		// Array with a length of 4
+		int[] marks = new int[5];
 		
-	
 		// Iterate array using for loop
 		for (int i = 0; i < marks.length; i++) {
 			int m = marks[i];
-			System.out.println("Mark is: " + m);
+			System.out.println("Marks " + i + " is : " + m);
 		}
-
+		// assign marks in the array
+		marks[0] = 50;
+//		marks[1] = 60;
+		marks[2] = 70;
+//		marks[3] = 55;
+// 		marks[4] = 80;	// ArrayIndexOutOfBounds Exception
+		// find how many items in the array
+		System.out.println("Array size is: " + marks.length);
+		// Iterate array using for loop
+		// TODO: Add conditionals so that it prints [50, 0, 70, 0]
+		for (int i = 0; i < marks.length; i++) {
+			int m = marks[i];
+//			System.out.println("Mark is: " + m);
+			System.out.print(m + ",");
+		}
+//System.out.println(Arrays.toString(marks));
 	}
-
 }
+
+
+
