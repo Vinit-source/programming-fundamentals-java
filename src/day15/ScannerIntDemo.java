0b5376a5534f9fad2008b450899049da4b13074d
Vinit Gore <vinitgore1234@gmail.com>
2023-06-21 10:00:55 +0530

    Day 15

--- src/day15/ScannerIntDemo.java
+++ src/day15/ScannerIntDemo.java
@@ -7,9 +7,9 @@ public class ScannerIntDemo {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter a your age: ");
-		int name = scanner.nextInt();
-		System.out.println("Your age is: " + name);
+		System.out.print("Enter a your age: ");
+		int age = scanner.nextInt();
+		System.out.println("Your age is: " + age);
 		scanner.close();
 	}
 
