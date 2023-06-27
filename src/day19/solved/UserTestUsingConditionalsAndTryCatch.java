9fabcfc7ab2c622579b54c26008466d25d5782cc
Vinit Gore <vinitgore1234@gmail.com>
2023-06-27 10:21:39 +0530

    Update UserTestUsingConditionalsAndTryCatch.java

--- src/day19/solved/UserTestUsingConditionalsAndTryCatch.java
+++ src/day19/solved/UserTestUsingConditionalsAndTryCatch.java
@@ -15,31 +15,58 @@ public class UserTestUsingConditionalsAndTryCatch {
 		// Correct case
 		System.out.println("Vinit@1234");
 		user.setPassword("Vinit@1234");
-		user.validatePassword();
+		if (user.validatePassword()) {
+			System.out.println("PASS");
+		} else {
+			System.out.println("FAIL");
+		};
+		
+		
+		
 		
 		// Capital letter missing
 		System.out.println("vinit@1234");
 		user.setPassword("vinit@1234");
-		user.validatePassword();
+		if (user.validatePassword()) {
+			System.out.println("FAIL");
+		} else {
+			System.out.println("PASS");
+		};
 
 		// Small letter missing
 		System.out.println("VINIT@1234");
 		user.setPassword("VINIT@1234");
-		user.validatePassword();
+		if (user.validatePassword()) {
+			System.out.println("FAIL");
+		} else {
+			System.out.println("PASS");
+		};
 		
 		// Special character missing
 		System.out.println("Vinit1234");
 		user.setPassword("Vinit1234");
-		user.validatePassword();
+		if (user.validatePassword()) {
+			System.out.println("FAIL");
+		} else {
+			System.out.println("PASS");
+		};
 		
 		// Length < 8
 		System.out.println("Abc@123");
 		user.setPassword("Abc@123");
-		user.validatePassword();
+		if (user.validatePassword()) {
+			System.out.println("FAIL");
+		} else {
+			System.out.println("PASS");
+		};
 
 		// Number missing
 		System.out.println("Vinit@Gore");
 		user.setPassword("Vinit@Gore");
-		user.validatePassword();
+		if (user.validatePassword()) {
+			System.out.println("FAIL");
+		} else {
+			System.out.println("PASS");
+		};
 	}
 }
